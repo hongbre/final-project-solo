45a349f2df238f6d55e18ceeea117d3a4dd81cb0
hongbre <hongbre@gmail.com>
2023-06-23 03:10:02 +0900

    ADD: DB init

--- enterwait/app.js
+++ enterwait/app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { connection } = require('./module/mysql');
+const { createTables } = require('./module/init');
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.status(200).send('Hello, World!');
 });
+
+app.get('/admin/init', connection, async (req, res) => {
+  try {
+    const [ createResults ] = await req.connection.query(createTables());
+    res.send('DB 초기화에 성공하였습니다.');
+  } catch (err) {
+    console.log(err);
+    res.send('DB 초기화에 실패하였습니다.');
+  }
+}); 
 
 app.use(express.json());
 
