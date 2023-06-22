fb657f5ad1dc0d5d29dd89c755597653b5e27872
hongbre <hongbre@gmail.com>
2023-06-23 02:36:45 +0900

    MODIFY: process.env.AWS_REGION to REGION

--- nursery/module/sqs.js
+++ nursery/module/sqs.js
@@ -1,7 +1,7 @@
 const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs');
 
 const sqsClient = new SQSClient({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
