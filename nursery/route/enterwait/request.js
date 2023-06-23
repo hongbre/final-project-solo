5b7935d475b8721eebc1083c58dc825ab4fbc42a
hongbre <hongbre@gmail.com>
2023-06-23 11:52:49 +0900

    MODIFY: /enterwait/request on nursery

--- nursery/route/enterwait/request.js
+++ nursery/route/enterwait/request.js
@@ -4,6 +4,7 @@ const { connection, queries: { insertRequest } } = require('../../module/mysql')
 router.post('/', connection, async (req, res) => {
   try {
     const [ insertResult ] = await req.connection.query(insertRequest(req.body));
+    console.log(insertResult);
     res.send('Success!');
   } catch (err) {
     console.log(err);
