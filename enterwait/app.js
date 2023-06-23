971c2004f236453abf2d19d2d8ce1045ea0425f6
hongbre <hongbre@gmail.com>
2023-06-23 09:34:49 +0900

    MODIFY: enterwait DB init

--- enterwait/app.js
+++ enterwait/app.js
@@ -2,24 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const { connection } = require('./module/mysql');
-const { createTables } = require('./module/init');
-
 app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
 });
 
-app.get('/admin/init', connection, async (req, res) => {
-  try {
-    const [ createResults ] = await req.connection.query(createTables());
-    res.send('DB 초기화에 성공하였습니다.');
-  } catch (err) {
-    console.log(err);
-    res.send('DB 초기화에 실패하였습니다.');
-  }
-}); 
-
 app.use(express.json());
+
+app.use('/admin/init', 'require('./route/admin/init'));
 
 app.use('/enterwait/request', require('./route/enterwait/request'));
 app.use('/enterwait/cancel', require('./route/enterwait/cancel'));
--- enterwait/module/mysql.js
+++ enterwait/module/mysql.js
@@ -74,11 +74,90 @@ const updateRequest = (body) => `
    WHERE request_id = '${body.request_id}'
 `;
 
+const cRequest = () => `
+  CREATE TABLE \`request\` (
+    \`request_id\` varchar(100) NOT NULL,
+    \`request_status\` int NOT NULL,
+    \`child_id\` varchar(100) NOT NULL,
+    \`child_name\` varchar(100) NOT NULL,
+    \`child_birthday\` varchar(100) NOT NULL,
+    \`user_id\` varchar(100) NOT NULL,
+    \`parent_name\` varchar(100) NOT NULL,
+    \`parent_tel\` varchar(100) NOT NULL,
+    \`parent_address\` varchar(150) NOT NULL,
+    \`parent_postcode\` varchar(5) NOT NULL,
+    \`parent_email\` varchar(100) NOT NULL,
+    \`nursery_id\` varchar(100) NOT NULL,
+    \`create_date\` timestamp NOT NULL,
+    \`change_date\` timestamp NOT NULL,
+
+    PRIMARY KEY (
+      \`request_id\`
+    )
+  );
+`;
+
+const cNursery = () => `
+  CREATE TABLE \`nursery\` (
+    \`nursery_id\` varchar(100) NOT NULL,
+    \`nursery_password\` varchar(100) NOT NULL,
+    \`nursery_name\` varchar(100) NOT NULL,
+    \`nursery_tel\` varchar(100) NOT NULL,
+    \`postcode\` varchar(5) NOT NULL,
+    \`address\` varchar(150) NOT NULL,
+    \`email\` varchar(100) NOT NULL,
+    \`capacity\` int NOT NULL,
+    \`create_date\` timestamp NOT NULL,
+    \`change_date\` timestamp NOT NULL,
+
+    PRIMARY KEY (
+      \`nursery_id\`
+    )
+  );
+`);
+
+const cChild = () => `
+  CREATE TABLE \`child\` (
+    \`child_id\` int NOT NULL,
+    \`child_name\` varchar(20) NOT NULL,
+    \`child_birthday\` varchar(10) NOT NULL,
+    \`user_id\` int(15) NOT NULL,
+    \`create_date\` timestamp NOT NULL,
+    \`change_date\` timestamp NOT NULL,
+
+    PRIMARY KEY (
+      \`child_id\`
+    )
+  );
+`;
+
+const cUser = () => `
+  CREATE TABLE \`user\` (
+    \`user_id\` varchar(15) NOT NULL,
+    \`user_password\` varchar(20) NOT NULL,
+    \`user_name\` varchar(15) NOT NULL,
+    \`user_brithday\` varchar(10) NOT NULL,
+    \`user_postcode\` int NOT NULL,
+    \`user_address\` varchar(50) NOT NULL,
+    \`create_date\` timestamp NOT NULL,
+    \`change_date\` timestamp NOT NULL,
+
+    PRIMARY KEY (
+      \`user_id\`
+    )
+  );
+`;
+
 module.exports = {
   connection,
   queries: {
     selectRequest,
     insertRequest,
-    updateRequest
+    updateRequest,
+    cRequest,
+    cNursery,
+    cChild,
+    cUser
   }
 };
+
